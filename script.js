fb5fd9d32824a30eab382ea8e9aee71411c34b06
Syeda Amna Raza <76561718+SAR-hub@users.noreply.github.com>
2022-08-29 12:16:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
     for (var i=0;i<names.length;i++){
       var firstletter= names[i].charAt(0).toLowerCase();
       if (firstletter==="j"){
-        byeSpeaker.speak(names[i]);
+        byeSpeaker(names[i]);
       }
       else{
-        helloSpeaker.speak(names[i]);
+        helloSpeaker(names[i]);
       }
     }
 })();
